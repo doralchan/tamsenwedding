732a2840f30e999bd499e3f9745de365b84f0940
Dora Chan <dora.l.chan@gmail.com>
2019-09-10 07:29:00 -0700

    pushing last changes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d1a6cb0255f76b370dbb";
+/******/ 	var hotCurrentHash = "812ca275340b27ebff62";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
